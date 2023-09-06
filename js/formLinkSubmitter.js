2de538b39544fbea6afba0da4269d13eb16d5c2d
Jyotirmay Chowdhury <chowdhuryjyobad21hbcs@student.mes.ac.in>
2023-09-06 17:04:34 +0530

    Commit

---
+++ js/formLinkSubmitter.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    document.getElementById("submitForm").addEventListener("click", function(event){
+        event.preventDefault()
+        let link = document.getElementById("link").value
+        let linkFinal = document.getElementById("link").value.replace("https://youtu.be/","")
+        if(link == ""){
+            console.log("no link")
+        }else{
+            let video = document.getElementById("videoPlayer")
+            video.setAttribute("src", "https://www.youtube.com/embed/"+linkFinal)
+            sendURL(link)
+        }
+    })
+}
+function sendURL(URL){
+    window.location.href = `https://ztube.glitch.me/download?URL=${URL}`
+}
